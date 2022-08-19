2e1e01d137b848ae7d18f512fcbb0595ee3c1f05
Bage <bagerathi.s@gmail.com>
2022-08-19 08:45:02 +0530

    files changed

---
+++ Simplilearnproj1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplilearnproj1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"D:\simplilearnproject1\test.txt";
+
+            //  string[] lines = File.ReadAllines(filepath);
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filepath).ToList();
+
+              foreach  (string line in lines)
+             {
+
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
