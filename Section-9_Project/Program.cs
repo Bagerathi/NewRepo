f9f807227348142cfaf0f2c37bc6898da6517bc1
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-09-15 19:10:43 +0530

    Main Program

---
+++ Section-9_Project/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section9_simplilearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            s.gets();
+            s.prints();
+            s = new teacher();
+            s.gets();
+            s.prints();
+            s = new subject();
+            s.gets();
+            s.prints();
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
